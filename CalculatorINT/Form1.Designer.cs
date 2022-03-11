6492afd7ffc58d2ffa291d688b03c929f48e7009
estm3 <60245369+estm3@users.noreply.github.com>
2022-03-10 19:43:21 -0600

    Add files via upload

---
+++ CalculatorINT/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CalculatorINT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.resulCU = new System.Windows.Forms.Label();
+            this.resulT = new System.Windows.Forms.Label();
+            this.radio = new System.Windows.Forms.TextBox();
+            this.resulC = new System.Windows.Forms.Label();
+            this.lado = new System.Windows.Forms.TextBox();
+            this.bas = new System.Windows.Forms.TextBox();
+            this.altu = new System.Windows.Forms.TextBox();
+            this.circulo = new System.Windows.Forms.Button();
+            this.cuadrado = new System.Windows.Forms.Button();
+            this.triangu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(322, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Area Circulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(293, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Area Cuadrado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(294, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Area Triangulo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(246, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Radio: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(247, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Base";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(247, 416);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Altura";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(246, 232);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 21);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Lado: ";
+            // 
+            // resulCU
+            // 
+            this.resulCU.AutoSize = true;
+            this.resulCU.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resulCU.Location = new System.Drawing.Point(493, 229);
+            this.resulCU.Name = "resulCU";
+            this.resulCU.Size = new System.Drawing.Size(19, 21);
+            this.resulCU.TabIndex = 8;
+            this.resulCU.Text = "0";
+            // 
+            // resulT
+            // 
+            this.resulT.AutoSize = true;
+            this.resulT.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resulT.Location = new System.Drawing.Point(494, 391);
+            this.resulT.Name = "resulT";
+            this.resulT.Size = new System.Drawing.Size(19, 21);
+            this.resulT.TabIndex = 9;
+            this.resulT.Text = "0";
+            // 
+            // radio
+            // 
+            this.radio.Location = new System.Drawing.Point(328, 87);
+            this.radio.Name = "radio";
+            this.radio.Size = new System.Drawing.Size(100, 20);
+            this.radio.TabIndex = 10;
+            // 
+            // resulC
+            // 
+            this.resulC.AutoSize = true;
+            this.resulC.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resulC.Location = new System.Drawing.Point(493, 87);
+            this.resulC.Name = "resulC";
+            this.resulC.Size = new System.Drawing.Size(19, 21);
+            this.resulC.TabIndex = 11;
+            this.resulC.Text = "0";
+            // 
+            // lado
+            // 
+            this.lado.Location = new System.Drawing.Point(327, 229);
+            this.lado.Name = "lado";
+            this.lado.Size = new System.Drawing.Size(100, 20);
+            this.lado.TabIndex = 12;
+            // 
+            // bas
+            // 
+            this.bas.Location = new System.Drawing.Point(328, 366);
+            this.bas.Name = "bas";
+            this.bas.Size = new System.Drawing.Size(100, 20);
+            this.bas.TabIndex = 13;
+            // 
+            // altu
+            // 
+            this.altu.Location = new System.Drawing.Point(328, 413);
+            this.altu.Name = "altu";
+            this.altu.Size = new System.Drawing.Size(100, 20);
+            this.altu.TabIndex = 14;
+            // 
+            // circulo
+            // 
+            this.circulo.Location = new System.Drawing.Point(339, 131);
+            this.circulo.Name = "circulo";
+            this.circulo.Size = new System.Drawing.Size(75, 23);
+            this.circulo.TabIndex = 15;
+            this.circulo.Text = "Calcular";
+            this.circulo.UseVisualStyleBackColor = true;
+            this.circulo.Click += new System.EventHandler(this.Circulo_Click);
+            // 
+            // cuadrado
+            // 
+            this.cuadrado.Location = new System.Drawing.Point(339, 277);
+            this.cuadrado.Name = "cuadrado";
+            this.cuadrado.Size = new System.Drawing.Size(75, 23);
+            this.cuadrado.TabIndex = 16;
+            this.cuadrado.Text = "Calcular";
+            this.cuadrado.UseVisualStyleBackColor = true;
+            this.cuadrado.Click += new System.EventHandler(this.Cuadrado_Click);
+            // 
+            // triangu
+            // 
+            this.triangu.Location = new System.Drawing.Point(339, 450);
+            this.triangu.Name = "triangu";
+            this.triangu.Size = new System.Drawing.Size(75, 23);
+            this.triangu.TabIndex = 17;
+            this.triangu.Text = "Calcular";
+            this.triangu.UseVisualStyleBackColor = true;
+            this.triangu.Click += new System.EventHandler(this.Triangu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(746, 485);
+            this.Controls.Add(this.triangu);
+            this.Controls.Add(this.cuadrado);
+            this.Controls.Add(this.circulo);
+            this.Controls.Add(this.altu);
+            this.Controls.Add(this.bas);
+            this.Controls.Add(this.lado);
+            this.Controls.Add(this.resulC);
+            this.Controls.Add(this.radio);
+            this.Controls.Add(this.resulT);
+            this.Controls.Add(this.resulCU);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label resulCU;
+        private System.Windows.Forms.Label resulT;
+        private System.Windows.Forms.TextBox radio;
+        private System.Windows.Forms.Label resulC;
+        private System.Windows.Forms.TextBox lado;
+        private System.Windows.Forms.TextBox bas;
+        private System.Windows.Forms.TextBox altu;
+        private System.Windows.Forms.Button circulo;
+        private System.Windows.Forms.Button cuadrado;
+        private System.Windows.Forms.Button triangu;
+    }
+}
+
---
+++ CalculatorINT/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorINT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Circulo_Click(object sender, EventArgs e)
+        {
+            AreasServiceReference.AreasWebServiceSoapClient ws = new AreasServiceReference.AreasWebServiceSoapClient();
+            resulC.Text = ws.AreaCirculo(int.Parse(radio.Text));
+        }
+
+        private void Cuadrado_Click(object sender, EventArgs e)
+        {
+            AreasServiceReference.AreasWebServiceSoapClient ws = new AreasServiceReference.AreasWebServiceSoapClient();
+            resulCU.Text = ws.AreaCuadrado(int.Parse(lado.Text));
+
+        }
+
+        private void Triangu_Click(object sender, EventArgs e)
+        {
+            AreasServiceReference.AreasWebServiceSoapClient ws = new AreasServiceReference.AreasWebServiceSoapClient();
+            resulT.Text = ws.AreaTriangulo(int.Parse(bas.Text), int.Parse(altu.Text));
+        }
+    }
+}
